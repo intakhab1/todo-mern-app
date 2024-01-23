054a25e5df408f2d67b4e95dde3f36cb8be50c71
Mohd Intakhab Alam <amirint190@gmail.com>
2024-01-23 20:29:15 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,5 +2,6 @@
 // export const baseURL = `http://localhost:4000/api/v1`;
 
 // for running on render 
-export const baseURL = `https://todo-backend-irel.onrender.com/api/v1`;
+// export const baseURL = `https://todo-backend-irel.onrender.com/api/v1`;
+export const baseURL = process.env.REACT_APP_BASE_URL;
 
